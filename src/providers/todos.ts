f6a09b3b96b9946099189f2cb5b3ad7d70d48a78
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-07 10:24:13 -0800

    Updates todos provider to make room for user auth

--- src/providers/todos.ts
+++ src/providers/todos.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import PouchDB from 'pouchdb';
 
-/*
-  Generated class for the Todos provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class Todos {
+
   data: any;
   db: any;
   remote: any;
 
   constructor() {
+
+  }
+
+  init(details){
+
     this.db = new PouchDB('cloudo');
 
-    this.remote = 'http://localhost:5984/cloudo';
+    this.remote = details.userDBs.supertest;
 
     let options = {
       live: true,
@@ -25,17 +25,34 @@ export class Todos {
     };
 
     this.db.sync(this.remote, options);
+
+    console.log(this.db);
+
+  }
+
+  logout(){
+
+    this.data = null;
+
+    this.db.destroy().then(() => {
+      console.log("database removed");
+    });
   }
 
   getTodos() {
+
     if (this.data) {
       return Promise.resolve(this.data);
     }
 
     return new Promise(resolve => {
+
       this.db.allDocs({
+
         include_docs: true
+
       }).then((result) => {
+
         this.data = [];
 
         result.rows.map((row) => {
@@ -47,50 +64,65 @@ export class Todos {
         this.db.changes({live: true, since: 'now', include_docs: true}).on('change', (change) => {
           this.handleChange(change);
         });
+
       }).catch((error) => {
+
         console.log(error);
+
       });
+
     });
+
   }
 
-  createTodo(todo) {
+  createTodo(todo){
     this.db.post(todo);
   }
 
-  updateTodo(todo) {
+  updateTodo(todo){
     this.db.put(todo).catch((err) => {
       console.log(err);
     });
   }
 
-  deleteTodo(todo) {
+  deleteTodo(todo){
     this.db.remove(todo).catch((err) => {
       console.log(err);
     });
   }
 
-  handleChange(change) {
+  handleChange(change){
+
     let changedDoc = null;
     let changedIndex = null;
 
     this.data.forEach((doc, index) => {
-      if (doc._id === change.id) {
+
+      if(doc._id === change.id){
         changedDoc = doc;
         changedIndex = index;
       }
+
     });
 
-    // a document was deleted
-    if (change.deleted) {
+    //A document was deleted
+    if(change.deleted){
       this.data.splice(changedIndex, 1);
-    } else {
-      // a document was updated
-      if (changedDoc) {
+    }
+    else {
+
+      //A document was updated
+      if(changedDoc){
         this.data[changedIndex] = change.doc;
-      } else {
-        // a document was added
+      }
+
+      //A document was added
+      else {
         this.data.push(change.doc);
       }
+
     }
+
   }
+
 }
