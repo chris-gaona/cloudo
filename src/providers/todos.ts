aba916107ec9004081f62078562509accd09a47d
Chris Gaona <thebeloved88@sbcglobal.net>
2017-01-06 17:02:29 -0800

    Finishes up provider functions for CRUD operations to pouchdb

--- src/providers/todos.ts
+++ src/providers/todos.ts
@@ -28,22 +28,69 @@ export class Todos {
   }
 
   getTodos() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
 
+    return new Promise(resolve => {
+      this.db.allDocs({
+        include_docs: true
+      }).then((result) => {
+        this.data = [];
+
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({live: true, since: 'now', include_docs: true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
   }
 
   createTodo(todo) {
-
+    this.db.post(todo);
   }
 
   updateTodo(todo) {
-
+    this.db.put(todo).catch((err) => {
+      console.log(err);
+    });
   }
 
   deleteTodo(todo) {
-
+    this.db.remove(todo).catch((err) => {
+      console.log(err);
+    });
   }
 
   handleChange(change) {
+    let changedDoc = null;
+    let changedIndex = null;
 
+    this.data.forEach((doc, index) => {
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+    });
+
+    // a document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    } else {
+      // a document was updated
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      } else {
+        // a document was added
+        this.data.push(change.doc);
+      }
+    }
   }
 }
